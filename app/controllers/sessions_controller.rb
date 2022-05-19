09d06ea260f79f67e38f6a30522d3ccfd82642b2
shuhei_hagino <shuhei_hagino@mediado.jp>
2022-05-19 16:59:34 +0900

    1

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+
+    def new
+    end
+
+    def destroy
+      session.delete(:user_id)
+      flash[:notice] = 'ログアウトしました'
+      redirect_to new_session_path
+    end
+
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user&.authenticate(params[:session][:password])
+          log_in(user)
+          redirect_to user_path(user.id)
+          else
+            flash.now[:danger] = 'ログインに失敗しました'
+            render :new
+          end 
+    end
+
+
+end
